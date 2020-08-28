5c9c378446a7cceee9c6b00919bcbac985758f49
Meglazero <alexander.vanmeter@gmail.com>
2020-08-28 02:06:38 -0500

    Trying to fix images

--- app.js
+++ app.js
@@ -2,12 +2,12 @@ function showMtn1() {
     document.querySelector('.mtn1').style.display = ''
     document.querySelector('.btn1').id = 'selected'
     document.querySelector('.mtn2').style.display = 'none'
-    document.querySelector('.btn2').id = ''
+    document.querySelector('.btn2').id = 'none'
 }
 
 function showMtn2() {
     document.querySelector('.mtn2').style.display = ''
     document.querySelector('.btn2').id = 'selected'
     document.querySelector('.mtn1').style.display = 'none'
-    document.querySelector('.btn1').id = ''
+    document.querySelector('.btn1').id = 'none'
 }
